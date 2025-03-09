5f546de15a76597c4a0d0ebaed7e86bc96638dd7
YasasriBanuka <banukadias5@gmail.com>
2025-03-09 21:58:38 +0530

    Booking Repo new changes

--- src/main/java/com/megacity/server/Repository/BookingRepository.java
+++ src/main/java/com/megacity/server/Repository/BookingRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.megacity.server.Model.Booking;
-
+// 
 @Repository
 public interface BookingRepository extends MongoRepository<Booking , String>{
 
